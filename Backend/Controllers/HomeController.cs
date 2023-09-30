196a65dec07da2f1327f072fc32fc6416677b110
javidbs <javidbs@TITAN06>
2023-09-30 12:48:24 +0400

    startup

--- Backend/Controllers/HomeController.cs
+++ Backend/Controllers/HomeController.cs
@@ -1,9 +1,16 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Backend.DAL;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Backend.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly AppDbContext _appDbContext;
+
+        public HomeController(AppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
         public IActionResult Index()
         {
             return View();

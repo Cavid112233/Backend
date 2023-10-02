a84ef3da38063fe2e60250c413b10ff16f241994
Cavid112233 <suleymanli535@gmail.com>
2023-10-02 19:06:56 +0400

    settings

---
+++ Backend/ViewComponent/FooterViewComponent.cs
@@ -0,0 +1,25 @@
+﻿using Backend.Controllers;
+using Backend.DAL;
+using Backend.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Backend.ViewComponents
+{
+    public class FooterViewComponent : ViewComponent
+    {
+        public readonly AppDbContext _context;
+
+        public FooterViewComponent(AppDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            HomeVM vm = new HomeVM();
+            {
+                Settings = _context.Settings.ToDictionary(s => s.Key, s => s.Value);
+            };
+            return View(await Task.FromResult(vm));
+        }
+    }
+}
--- Backend/ViewModels/HomeVM.cs
+++ Backend/ViewModels/HomeVM.cs
@@ -6,6 +6,7 @@ namespace Backend.ViewModels
 {
     public class HomeVM
     {
+        public Dictionary<string, string> Settings { get; set; }
         public List<SliderContent> SliderContents { get; set; }
         public List<Notice> Notices { get; set; }
     }
